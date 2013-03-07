2616e5273635edd97f72de80cb26147068cd21e1
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2013-03-07 20:42:41 +0000

    solved the problem that the GPL 2 is incompatible with the APL 2.0 (which is used by ACRA) by updating the project to GPL 3
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/checknetwork/trunk@190 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/org/zephyrsoft/checknetwork/util/Logger.java
+++ src/org/zephyrsoft/checknetwork/util/Logger.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of TrackWorkTime (TWT).
+ * This file is part of CheckNetwork (CN).
  *
- * TWT is free software: you can redistribute it and/or modify it
+ * CN is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TWT is distributed in the hope that it will be useful, but
+ * CN is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with TWT. If not, see <http://www.gnu.org/licenses/>.
+ * along with CN. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.zephyrsoft.checknetwork.util;
 
